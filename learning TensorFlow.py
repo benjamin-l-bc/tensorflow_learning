6b616367664ee36b0d50023ea5cf85a1948504f8
benjaminshi02003220 <346473509@qq.com>
2018-04-05 15:29:36 +0800

    v1.1

--- learning TensorFlow.py
+++ learning TensorFlow.py
@@ -1,9 +1,35 @@
 import tensorflow as tf
-w1=tf.Variable(tf.random_normal([2,3]))
-w2=tf.Variable(tf.random_normal([3,1]))
-x=tf.constant([[2.0,1.0]])
+from numpy.random import RandomState
+batch_size=20
+w1=tf.Variable(tf.random_normal([2,3],seed=1))
+w2=tf.Variable(tf.random_normal([3,1],seed=1))
+x=tf.placeholder(tf.float32,name='x-input')
+y_=tf.placeholder(tf.float32,name='y-input')
+
 a=tf.matmul(x,w1)
 y=tf.matmul(a,w2)
-sess=tf.Session()
-sess.run(tf.global_variables_initializer())
-print(sess.run(y))
+cross_entropy=-tf.reduce_mean(y_*tf.log(tf.clip_by_value(y,1e-10,1.0)))
+train_step=tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+
+rdm=RandomState(1)
+dataset_size=128
+X=rdm.rand(dataset_size,2)
+Y=[[int(x1+x2<1)] for (x1,x2) in X]
+
+with tf.Session() as sess:
+    init_op=tf.global_variables_initializer()
+    sess.run(init_op)
+    print(sess.run(w1))
+    print(sess.run(w2))
+    
+    STEPS=10000
+    for i in range(STEPS):
+        start=(i*batch_size)%dataset_size
+        end=min(start+batch_size,dataset_size)
+    
+        sess.run(train_step,feed_dict={x:X[start:end],y_:Y[start:end]})
+        if i%1000==0:
+            total_cross_entropy=sess.run(cross_entropy,feed_dict={x:X,y_:Y})
+            print("After {0} training steps,cross entropy on all data is {1}".format(i,total_cross_entropy))
+            print(sess.run(w1))
+            print(sess.run(w2))
